87bf9c9b57bc0380cb2d020f43b0ebf6cae47b2b
Harichandana Neralla <harichandana10899@gmail.com>
2022-10-24 07:14:56 -0500

    first draft of ui

---
+++ nft-transaction-ui/src/App.svelte
@@ -0,0 +1,89 @@
+<!-- <script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+    import Login from './login.svelte';
+</script>
+
+<main>
+  <div>
+    <a href="/login">Go to Login page</a>
+    <a href="https://vitejs.dev" target="_blank"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>  -->
+
+<script>
+  import { onMount, setContext } from "svelte";
+  import {
+    key as userContextKey,
+    initialValue as userContextInitialValue
+  } from "./userContext";
+
+  import LoginForm from "./login.svelte";
+
+  onMount(() => {
+    setContext(userContextKey, userContextInitialValue);
+  });
+
+  const submit = ({ email, password }) =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        setContext(userContextKey, {
+          name: "Foo",
+          lastName: "Bar",
+          email: "foo@bar.com"
+        });
+        resolve();
+      }, 1000);
+    });
+</script>
+
+<style>
+  section {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(to right, #0d0a0d, #dc76ba);
+  }
+</style>
+<!-- <main> -->
+  <section>
+    <LoginForm {submit} />
+  </section>
+<!-- </main> -->
